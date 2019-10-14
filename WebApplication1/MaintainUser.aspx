56c79df8403cb8ccac3d2a575ad271b4578e53ad
Hung Bui <buisihung321@gmail.com>
2019-10-14 23:16:02 +0700

    Add project files.

---
+++ WebApplication1/MaintainUser.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" %>
+<%@ Import Namespace="WebApplication1" %>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<script runat="server">
+
+    UserInfoData data = new UserInfoData();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+            LoadData();
+    }
+
+    public void LoadData()
+    {
+        gvUserInfoList.DataSource = data.GetUserList();
+        gvUserInfoList.DataBind();
+
+    }
+
+  
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        dynamic u = new UserInfo
+        {
+            Username = txtUserName.Text,
+            Password = txtPassword.Text,
+            Address = txtAddress.Text,
+            Email = txtEmail.Text,
+            BirthDate = DateTime.Parse(txtBirthDate.Text)
+        };
+        data.InsertUserInfo(u);
+        LoadData();
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        dynamic u = new UserInfo {Username = txtUserName.Text};
+        data.Delete(u);
+        LoadData();
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        dynamic u = new UserInfo
+        {
+            Username = txtUserName.Text,
+            Password = txtPassword.Text,
+            Address = txtAddress.Text,
+            Email = txtEmail.Text,
+            BirthDate = DateTime.Parse(txtBirthDate.Text)
+        };
+        data.UpdateUserInfo(u);
+        LoadData();
+    }
+
+    protected void gvUserInfoList_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+    {
+        GridViewRow gv = gvUserInfoList.Rows[e.NewSelectedIndex];
+        txtUserName.Text = gv.Cells[1].Text;
+        txtPassword.Text = gv.Cells[2].Text;
+        txtBirthDate.Text = gv.Cells[3].Text;
+        txtAddress.Text = gv.Cells[4].Text;
+        txtEmail.Text = gv.Cells[5].Text;
+    }
+</script><title>Title</title>
+</head>
+<body>
+<form id="HtmlForm" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label>
+        <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
+        <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="BirthDate"></asp:Label>
+        <asp:TextBox ID="txtBirthDate" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Addresss"></asp:Label>
+        <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Email"></asp:Label>
+        <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+    </div>
+    <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+    <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+    <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+    <asp:GridView ID="gvUserInfoList" runat="server" AutoGenerateSelectButton="True" OnSelectedIndexChanging="gvUserInfoList_SelectedIndexChanging">
+    </asp:GridView>
+</form>
+</body>
+</html>
---
+++ WebApplication1/UserInfoData.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class UserInfoData
+    {
+        private string strConnection = $"server=.\\SQLEXPRESS;database=SaleApp;integrated security = true";
+
+        public UserInfoData()
+        {
+        }
+
+        public void InsertUserInfo(UserInfo userInfo)
+        {
+            SqlConnection cnn = new SqlConnection(strConnection);
+            string SQLInsert = "INSERT UserInfo values (@UserName,@Password,@BirthDate,@Address,@Email)";
+            SqlCommand cmd = new SqlCommand(SQLInsert,cnn);
+            cmd.Parameters.AddWithValue("@Username", userInfo.Username);
+            cmd.Parameters.AddWithValue("@Password", userInfo.Password);
+            cmd.Parameters.AddWithValue("@BirthDate", userInfo.BirthDate);
+            cmd.Parameters.AddWithValue("@Address", userInfo.Address);
+            cmd.Parameters.AddWithValue("@Email", userInfo.Email);
+
+            try
+            {
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex) { throw new Exception(ex.Message); }
+            finally{ cnn.Close();}
+        }
+
+        public void UpdateUserInfo(UserInfo userInfo)
+        {
+            SqlConnection cnn = new SqlConnection(strConnection);
+            string SQLInsert = "UPDATE UserInfo set [Password] = @Password, BirthDate = @BirthDate," +
+                               " [Address] = @Address, [Email] = @Email " +
+                               " WHERE Username = @Username";
+            SqlCommand cmd = new SqlCommand(SQLInsert, cnn);
+            cmd.Parameters.AddWithValue("@Username", userInfo.Username);
+            cmd.Parameters.AddWithValue("@Password", userInfo.Password);
+            cmd.Parameters.AddWithValue("@BirthDate", userInfo.BirthDate);
+            cmd.Parameters.AddWithValue("@Address", userInfo.Address);
+            cmd.Parameters.AddWithValue("@Email", userInfo.Email);
+
+            try
+            {
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+            finally { cnn.Close(); }
+        }
+
+        public void Delete(UserInfo userInfo)
+        {
+            SqlConnection cnn = new SqlConnection(strConnection);
+            string SQLInsert = "DELETE UserInfo WHERE Username = @Username";
+            SqlCommand cmd = new SqlCommand(SQLInsert, cnn);
+            cmd.Parameters.AddWithValue("@Username", userInfo.Username);
+            try
+            {
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+            finally { cnn.Close(); }
+        }
+
+        public List<UserInfo> GetUserList()
+        {
+            List<UserInfo> data = new List<UserInfo>();
+            SqlConnection cnn = new SqlConnection(strConnection);
+            string SQLInsert = "Select * from UserInfo";
+            SqlCommand cmd = new SqlCommand(SQLInsert, cnn);
+            try
+            {
+                cnn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        dynamic u = new UserInfo
+                        {
+                            Username = dr["UserName"].ToString(),
+                            Password = dr["Password"].ToString(),
+                            Address = dr["Address"].ToString(),
+                            Email = dr["Email"].ToString(),
+                            BirthDate = DateTime.Parse(dr["BirthDate"].ToString())
+                        };
+
+                        data.Add(u);
+                    }
+                    
+
+                }
+
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+            finally { cnn.Close(); }
+            return data;
+
+        }
+
+    }
+}
